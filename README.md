9a33c4a1cab742aebf282636092fd3d79716a5a9
jaeseokkong <wotjr5124@gmail.com>
2023-05-24 14:57:10 +0900

    Update nextauth

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## 개요
 ### Infomation
 넥플릭스 클론 코딩
+Next js를 활용하여 
 
 ## 시작가이드
 ### Requirements
@@ -15,9 +16,9 @@ For builng and running the application you need:
 <img src="https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white"/>
 
 ### Development
-<img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=white"/> <img src="https://img.shields.io/badge/typescript-3178C6?style=for-the-badge&logo=typescript&logoColor=white"/> <img src="https://img.shields.io/badge/node.js-339933?style=for-the-badge&logo=node.js&logoColor=white"/> <img src="https://img.shields.io/badge/mongodb-47A248?style=for-the-badge&logo=mongodb&logoColor=white"/> <img src="https://img.shields.io/badge/nextdotjs-000000?style=for-the-badge&logo=nextdotjs&logoColor=white"/> 
+<img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=white"/> <img src="https://img.shields.io/badge/typescript-3178C6?style=for-the-badge&logo=typescript&logoColor=white"/> <img src="https://img.shields.io/badge/node.js-339933?style=for-the-badge&logo=node.js&logoColor=white"/> <img src="https://img.shields.io/badge/mongodb-47A248?style=for-the-badge&logo=mongodb&logoColor=white"/> <img src="https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white"/> 
 
-### CSS
+### Style
 <img src="https://img.shields.io/badge/tailwindcss-06B6D4?style=for-the-badge&logo=tailwindcss&logoColor=white"/> 
 
 
---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,67 @@
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from 'bcrypt';
+
+import prismadb from '@/lib/prismadb'
+
+export default NextAuth({
+    providers : [
+        Credentials({
+            id : 'credenitals',
+            name : 'Credenitials',
+            credentials : {
+                email : {
+                    label : 'Email',
+                    type : 'text',
+                },
+                password : {
+                    label : 'Password',
+                    type : 'password'
+                }
+            },
+
+            async authorize(credentials) {
+                //이메일 or 비밀번호 미입력시
+                if(!credentials?.email || !credentials?.password){
+                    throw new Error("이메일과 비밀번호를 입력해주세요.")
+                }
+
+                //해당 이메일의 유저 정보 가져오기
+                const user = await prismadb.user.findUnique({
+                    where : {
+                        email : credentials.email
+                    }
+                });
+
+                //이메일에 해당하는 유저 정보가 없는 경우
+                if(!user || !user.hashedPassword){
+                    throw new Error('가입된 이메일이 없습니다.')
+                }
+
+                //입력한 비밀번호와 계정 비밀번호(암호화) 비교
+                const isCorrectPassword = await compare(
+                    credentials.password,
+                    user.hashedPassword
+                );
+
+                //다른 비밀번호를 입력한 경우
+                if(!isCorrectPassword){
+                    throw new Error("비밀번호가 맞지않습니다.")
+                }
+
+                return user;
+            }
+        })
+    ],
+    pages : {
+        signIn : '/auth',
+    },
+    debug : process.env.NODE_ENV === 'development', //오류 발생시 로그표시
+    session : {
+        strategy : 'jwt',
+    },
+    jwt : {
+        secret : process.env.NEXTAUTH_JWT_SECRET
+    },
+    secret : process.env.NEXTAUTH_SECRET,
+})
